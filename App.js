1f0b01c0ce18b1ec0df973c2ed4bfa0504aed7f2
bryzle <brandonlum17@gmail.com>
2024-11-11 08:43:38 -0500

    Initial Test Set up for WP to React Native

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useState,useEffect} from 'react';
+import { FlatList } from 'react-native-web';
 
 export default function App() {
+  const [allBlogs, setAllBlogs] = useState([]);
+  const getAllBlogs= () => {
+    var url = 'test.vipersurf.com/wp-json/wp/v2/posts?per_page=2';
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setAllBlogs(json);
+      })
+      .catch((error) => {
+        console.error(error);})};
+
+  console.log(allBlogs);
+
+    useEffect(() => {
+      getAllBlogs();
+    });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+      data={allBlogs}
+      keyExtractor={({id},index) => id}
+      renderItem={({item}) => (
+        <View>
+          <Text>Blog ID: {item.id}</Text>
+          <Text>Blog Title: {item.title.rendered}</Text>
+          <Text>Blog Content: {item.content.rendered}</Text>
+        </View>
+      )}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +40,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:50,
   },
 });
