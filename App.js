961b327b33842d062d38fa6117fc3667ad4defdb
BRANDON LUM <brandonlum17@gmail.com>
2024-11-18 18:04:23 -0500

    Set up pages to render using webview

--- App.js
+++ App.js
@@ -1,61 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {useState,useEffect} from 'react';
-import { FlatList } from 'react-native-web';
+import { StyleSheet, View, Text, Button, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import * as WebBrowser from "expo-web-browser";
 
 export default function App() {
-  const [allBlogs, setAllBlogs] = useState([]);
-  const getAllBlogs= () => {
-    var url = 'test.vipersurf.com/wp-json';
-    fetch(url,{
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setAllBlogs(JSON.stringify(json));
-        console.log(JSON.stringify(json));
-      })
-      .catch((error) => {
-        console.log(error);})};
-
-  console.log(allBlogs);
-
-    useEffect(() => {
-      getAllBlogs();
-    });
+  useEffect(() => {
+    // Automatically open the URL when the component mounts
+    WebBrowser.openBrowserAsync('https://test.vipersurf.com/dj-spinit-profile')
+      .catch((error) => console.log("Error opening WebBrowser:", error));
+  }, []);
 
   return (
-    console.log(allBlogs),
-   <text>Hello World</text>,
-  <div>{allBlogs}</div>)
-
-    {/* <View style={styles.container}>
-      <FlatList
-      data={allBlogs}
-      keyExtractor={({id},index) => id}
-      renderItem={({item}) => (
-        <View>
-          <Text>Blog ID: {item.id}</Text>
-          <Text>Blog Title: {item.title.rendered}</Text>
-          <Text>Blog Content: {item.content.rendered}</Text>
-        </View>
-      )}/>
-      <StatusBar style="auto" />
-    </View> */}
-  
+    <View style={styles.container}>
+      <Text>Loading Profile...</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop:50,
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  postContainer: {
+    padding: 16,
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
   },
 });
